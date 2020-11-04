bd98d7c9a6c367458f76cfc05482cd05a1def0f9
juanstelling <juan.stelling@gmail.com>
2020-11-04 23:01:43 +0000

    adjust pipe in gamedisplay in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    const bird = document.querySelector('.bird')
-    const pipe = document.querySelector('.pipe')
-    const gameDisplay = document.querySelector('.game')
-    const ground = document.querySelector('.ground')
+    const bird = document.getElementById('bird');
+    const pipe = document.getElementById('pipe')
+    const gameDisplay = document.getElementById('game');
+    const ground = document.getElementById('ground');
 
     let birdLeft = 150;
     let birdBottom = 300;
@@ -15,53 +15,49 @@ document.addEventListener('DOMContentLoaded', () => {
         bird.style.bottom = birdBottom + "px";
         bird.style.left = birdLeft + "px";
     }
-    let gametimerId = setInterval(startGame, 32) // How slow the bird is going down 
+    let gametimerId = setInterval(startGame, 20); // speed of bird
 
     // CONTROL SET TO SPACE KEY (= 32)
 // hier moet nog mobile touch bij 
     function control(e) {
         if (e.keyCode === 32) {
-            jump()
-        };
-    };
+            jump();
+        }
+    }
 
     // BIRD JUMPING
     function jump() {
         if (birdBottom < 480) birdBottom += 50;    // Bird can't jump higher than 480px & how much px the bird jumps 
         bird.style.bottom = birdBottom + "px";
-        console.log(birdBottom)
+        console.log(birdBottom);
     }
-    document.addEventListener('keyup', control)
+    document.addEventListener('keypress', control);
 
-    // PIPES
-    function pipes() {
+
+    // PIPES 
+    function generatePipe() { 
         let pipeLeft = 768;
         let randomheight = Math.random() * 60; // random heights for the pipe 
         let pipeBottom = randomheight;
-        pipe.style.bottom = pipeBottom + "px";
+        const pipe = document.createElement('div');
+        pipe.classList.add('pipe')
+        gameDisplay.appendChild(pipe)
         pipe.style.left = pipeLeft + "px";
+        pipe.style.bottom = pipeBottom + "px";
 
-        function movePipe() {
+        function movePipe(){
             pipeLeft -= 2;
             pipe.style.left = pipeLeft + "px";
 
-            if (pipeLeft === -60) {
-                clearInterval(timerId)
+            if (pipeLeft === 0) {
+                  clearInterval(timerId);
                 gameDisplay.removeChild(pipe);
             }
         }
-        let timerId = setInterval(movePipe, 15); // snelheid bird
-
+        let timerId = setInterval(movePipe, 10) // speed of pipe
+        setTimeout(generatePipe, 3000)
     }
-    pipes()
-
-   
-    
-
-  
-
-  
-   
+    generatePipe();
 
 });
 
